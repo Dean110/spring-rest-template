5ec1edfc3c58e4db25067b19c364438d6cd7641a
Benjamin Williams <deanpbr@gmail.com>
2019-07-05 01:27:07 -0400

    Added to skeletons

--- src/test/java/wcci/springresttemplate/integration_tests/ApplicationLayerTests.java
+++ src/test/java/wcci/springresttemplate/integration_tests/ApplicationLayerTests.java
@@ -2,13 +2,18 @@ package wcci.springresttemplate.integration_tests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class ApplicationLayerTests {
-    @Test
+	@Autowired
+	MockMvc mockMvc;
+	
+	@Test
     public void loadingTestContext(){
 
     }
--- src/test/java/wcci/springresttemplate/integration_tests/WebLayerTests.java
+++ src/test/java/wcci/springresttemplate/integration_tests/WebLayerTests.java
@@ -2,13 +2,17 @@ package wcci.springresttemplate.integration_tests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
 @RunWith(SpringRunner.class)
 public class WebLayerTests {
-
+	@Autowired
+	MockMvc	mockMvc;
+	
     @Test
     public void testContextLoads() {
 
